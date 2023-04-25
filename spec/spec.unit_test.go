34b53cf869e79c20e4be04a203be1ea20d4b8f7d
Marlon Henry Schweigert <marlon@schweigert.com.br>
2023-04-25 15:00:26 -0300

    spec: add specs for hooks methods

--- spec/spec.unit_test.go
+++ spec/spec.unit_test.go
@@ -36,5 +36,41 @@ func TestSpecUnitSuite(t *testing.T) {
 				return Expect(Inline(func() error { return nil }).Test().Title, To[string](BeEqual("inline")))
 			}),
 		),
+
+		Describe(
+			"BeforeAll",
+
+			Inline(func() error { return Expect(BeforeAll().IsContext(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(BeforeAll().IsHook(), To[bool](BeEqual(true))) }),
+			Inline(func() error { return Expect(BeforeAll().IsTest(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(BeforeAll().Hook().IsBeforeAllHookEngine(), To[bool](BeEqual(true))) }),
+		),
+
+		Describe(
+			"BeforeEach",
+
+			Inline(func() error { return Expect(BeforeEach().IsContext(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(BeforeEach().IsHook(), To[bool](BeEqual(true))) }),
+			Inline(func() error { return Expect(BeforeEach().IsTest(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(BeforeEach().Hook().IsBeforeEachHookEngine(), To[bool](BeEqual(true))) }),
+		),
+
+		Describe(
+			"DeferEach",
+
+			Inline(func() error { return Expect(DeferEach().IsContext(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(DeferEach().IsHook(), To[bool](BeEqual(true))) }),
+			Inline(func() error { return Expect(DeferEach().IsTest(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(DeferEach().Hook().IsDeferEachHookEngine(), To[bool](BeEqual(true))) }),
+		),
+
+		Describe(
+			"DeferAll",
+
+			Inline(func() error { return Expect(DeferAll().IsContext(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(DeferAll().IsHook(), To[bool](BeEqual(true))) }),
+			Inline(func() error { return Expect(DeferAll().IsTest(), To[bool](BeEqual(false))) }),
+			Inline(func() error { return Expect(DeferAll().Hook().IsDeferAllHookEngine(), To[bool](BeEqual(true))) }),
+		),
 	)
 }
